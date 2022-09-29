0d8172d4bf63d60cac0023c8ea761fb243a39534
João Victor da Silva <jv147002@gmail.com>
2022-09-29 16:58:30 -0300

    Terminando

--- src/br/senai/sp/jandira/model/Especialidade.java
+++ src/br/senai/sp/jandira/model/Especialidade.java
@@ -3,55 +3,67 @@ package br.senai.sp.jandira.model;
 import javax.swing.JOptionPane;
 
 public class Especialidade {
-	
-	private String nome;
-	private String descricao;  
-	
+
+    private  static int contador = 99;
+    private String nome;
+    private String descricao;
+    private Integer codigo;
+
 //	Construtores da classe
-	
-	public Especialidade(String nome) {
-		this.nome = nome;
-	
-	}
-	
-	public Especialidade(String nome, String descricao) {
-		this.nome = nome;
-		this.descricao = descricao;
-		
-	}
-	
-	public Especialidade() {
-		
-		
-	}
-	
-	
+    public Especialidade(String nome) {
+        this.nome = nome;
+        this.contador++;
+        this.codigo = contador;
+    }
+
+    public Especialidade(String nome, String descricao) {
+        this.nome = nome;
+        this.descricao = descricao;
+        this.contador++;
+        this.codigo = contador;
+    }
+
+    public Especialidade() {
+        this.contador++;
+        this.codigo = contador;
+    }
+
 //	Metodos de acesso aos atributos 
-	
-	public void setNome(String nome){
-		if (nome.length() >=3) {
-			this.nome = nome;
-		}else {
-			JOptionPane.showMessageDialog(null, nome + " não é um nome válido \nDeve conter pelo menos três letras");
-		}
-		
-	}
-	public String getNome() {
-		return nome;
-	}
-	public void setDescricao(String descricao) {
-		if(descricao.length() >=10) {
-			this.descricao = descricao;
-		}else {
-			JOptionPane.showMessageDialog(null, descricao + "A descrição deve ter no minimo 10 letras ");
-		}
-		
-		}
-	public String getDescricao() {
-		return descricao;
-	}
-	
-	
-	
-	
+
+    public Integer getCodigo() {
+        return codigo;
+    }
+    
+
+    public  int getContador() {
+        return contador;
+    }
+    
+    
+    public void setNome(String nome) {
+        if (nome.length() >= 3) {
+            this.nome = nome;
+        } else {
+            JOptionPane.showMessageDialog(null, nome + " não é um nome válido \nDeve conter pelo menos três letras");
+        }
+
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setDescricao(String descricao) {
+        if (descricao.length() >= 10) {
+            this.descricao = descricao;
+        } else {
+            JOptionPane.showMessageDialog(null, descricao + "A descrição deve ter no minimo 10 letras ");
+        }
+
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
 }
--- src/br/senai/sp/jandira/testes/TesteObjetos.java
+++ src/br/senai/sp/jandira/testes/TesteObjetos.java
@@ -8,71 +8,76 @@ import br.senai.sp.jandira.model.PlanoDeSaude;
 
 public class TesteObjetos {
 
-	public static void main(String[] args) {
-		Especialidade e1 = new Especialidade("Cardiologia");
-		e1.setNome("cardiologia");
-		e1.setDescricao("não deixa ter um ataque");
-		
-		Especialidade e2 = new Especialidade("Gastroenteologia");
-		e2.setNome("Gastroenteologia");
-		e2.setDescricao("Não deixa ficar com dor de barriga");
-		
-		Especialidade e4 = new Especialidade( "Otorrino", "Cuida do ouvido");
-				
-		Especialidade e3 = new Especialidade("Gastroenteologia");
-		e3.setNome("Qualquer coisa");
-		
-		ArrayList<Especialidade> especialidades = new ArrayList<>(); 
-		especialidades.add(e1);
-		especialidades.add(e2);
-		especialidades.add(e3);
-		System.out.println(especialidades.size());
-		
+    public static void main(String[] args) {
+        Especialidade e1 = new Especialidade("Cardiologia");
+        e1.setNome("cardiologia");
+        e1.setDescricao("não deixa ter um ataque");
+
+        Especialidade e2 = new Especialidade("Gastroenteologia");
+        e2.setNome("Gastroenteologia");
+        e2.setDescricao("Não deixa ficar com dor de barriga");
+
+        Especialidade e4 = new Especialidade("Otorrino", "Cuida do ouvido");
+
+        Especialidade e3 = new Especialidade("Gastroenteologia");
+        e3.setNome("Qualquer coisa");
+        
+        Especialidade e5 = new Especialidade("5");
+        e5.setNome("Especialidade 5");
+        
+        System.out.println("Total:"+e1.getContador());
+        System.out.println(e1.getCodigo() + "---" + e1.getNome());
+        System.out.println(e2.getCodigo() + "---" + e2.getNome());
+        System.out.println(e3.getCodigo() + "---" + e3.getNome());
+        System.out.println(e4.getCodigo() + "---" + e4.getNome());
+        System.out.println(e5.getCodigo() + "---" + e5.getNome());
+
+        ArrayList<Especialidade> especialidades = new ArrayList<>();
+        especialidades.add(e1);
+        especialidades.add(e2);
+        especialidades.add(e3);
+        System.out.println(especialidades.size());
+
 //		imprimir no console o nome das especialidades  que estão armazenadas no arrylist
-		
-		int i = 0;
-		while(i < especialidades.size()) {
-			System.out.println(especialidades.get(i).getNome());
-			i++;
-		} 
-		
+        int i = 0;
+        while (i < especialidades.size()) {
+            System.out.println(especialidades.get(i).getNome());
+            i++;
+        }
+
 //		 utilização do for para iteração
-		System.out.println("------------------for------------------------");
-		for(int x = 0; x < especialidades.size(); x++) {
-			System.out.println(especialidades.get(x).getNome());
-		}
-		
+        System.out.println("------------------for------------------------");
+        for (int x = 0; x < especialidades.size(); x++) {
+            System.out.println(especialidades.get(x).getNome());
+        }
+
 //		FOR  EACH -> para cada 
-		
-		System.out.println("-------------------for each-------------------");
-		for(Especialidade e : especialidades) {
-			System.out.println(e.getNome());
-		}
-		
+        System.out.println("-------------------for each-------------------");
+        for (Especialidade e : especialidades) {
+            System.out.println(e.getNome());
+        }
+
 //		 criar 3 palnos de saúde, armazenar em um arraylist e exibir o nome da operadora de cas um deles usando for each
-		
-		PlanoDeSaude p1 = new PlanoDeSaude("norte");
-		PlanoDeSaude p2 = new PlanoDeSaude("Sul");
-		PlanoDeSaude p3 = new PlanoDeSaude("Leste");
-		
-		ArrayList<PlanoDeSaude> planos = new ArrayList<>();
-		planos.add(p1);
-		planos.add(p2);
-		planos.add(p3);
-		
-		System.out.println("-------------------------------fiz sozinho -----------------------");
-		for(PlanoDeSaude e : planos) {
-			System.out.println(e.getOperadora());
-			System.out.println(e.getQuantidade());
-			
-		}
-		
-		
-		System.out.println("-----------------" +PlanoDeSaude.getQuantidade());
-		
-		
-		AgendaApp.main(args);
-		
-		System.out.println("-----------------" +PlanoDeSaude.getQuantidade());
-	}
+        PlanoDeSaude p1 = new PlanoDeSaude("norte");
+        PlanoDeSaude p2 = new PlanoDeSaude("Sul");
+        PlanoDeSaude p3 = new PlanoDeSaude("Leste");
+
+        ArrayList<PlanoDeSaude> planos = new ArrayList<>();
+        planos.add(p1);
+        planos.add(p2);
+        planos.add(p3);
+
+        System.out.println("-------------------------------fiz sozinho -----------------------");
+        for (PlanoDeSaude e : planos) {
+            System.out.println(e.getOperadora());
+            System.out.println(e.getQuantidade());
+
+        }
+
+        System.out.println("-----------------" + PlanoDeSaude.getQuantidade());
+
+        AgendaApp.main(args);
+
+        System.out.println("-----------------" + PlanoDeSaude.getQuantidade());
+    }
 }
